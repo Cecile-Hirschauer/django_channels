dc39b7a50087992687fd91c4fd72b293838b77e5
chirsch <cecile.h.75@gmail.com>
2021-07-05 08:25:47 +0200

    fix consumers.py

--- jokes_project/jokes/consumers.py
+++ jokes_project/jokes/consumers.py
@@ -2,11 +2,11 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 
 class JokeConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        await self.channel_layer.add_group('jokes', self.channel_name)
+        await self.channel_layer.group_add('jokes', self.channel_name)
         await self.accept()
         
     async def disconnect(self):
-        await self.channel_layer.discard_group('jokes', self.channel_name)
+        await self.channel_layer.group_discard('jokes', self.channel_name)
     
     async def send_jokes(self, event):
         text_message = event['text']
